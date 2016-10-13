8b0dbdfe3b2af653c25b3546fb5be16fa65b99ac
josivan <josivan>
2016-10-13 13:04:11 -0300

    Running Angular 2 with Electron.

---
+++ static/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const electron = require('electron');
+// const menu = require('./components/menu.app.js');
+// require('./components/events.app.js');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let window = null;
+
+app.on('ready', () => {
+  window = new BrowserWindow({
+    heigth: 600,
+    width: 800,
+    center: true
+  });
+  // window.setMenu(menu);
+  // window.loadURL(`file://${__dirname}/app/index.html`); 
+  window.loadURL(`file://${__dirname}/index.html`);
+  window.openDevTools();
+
+  /*
+  window.on('page-title-updated', (event, title) => {
+    event.preventDefault();
+  });
+  */
+
+  window.on('closed', () => {
+    window = null;
+  });
+});
